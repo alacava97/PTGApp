3e6a59a1cbb15065f67ab8ede4c58d72b9ee4fae
alacava97 <alacava97@gmail.com>
2025-11-06 20:19:55 -0500

    Update requireLogin.js

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,4 +1,8 @@
 function requireLogin(req, res, next) {
+  if (req.originalUrl.startsWith('/auth')) {
+    return next();
+  }
+  
   if (!req.session.user) {
     if (req.originalUrl.startsWith('/api')) {
       return res.status(401).json({ error: 'Unauthorized' });
