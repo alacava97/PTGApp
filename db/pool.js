30f5c018be863238faa8b36ddab9c0a4db75e86f
alacava97 <alacava97@gmail.com>
2026-03-13 15:32:09 -0400

    Update pool.js

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
 
   max: 10,
   idelTimeoutMillis: 10000,
