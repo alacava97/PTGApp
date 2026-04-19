a8935e00525724c39203d8a07dd0f2148a9a1cf0
alacava97 <alacava97@gmail.com>
2026-04-19 15:33:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1203,7 +1203,7 @@ app.post('/api/export-pdf/:filename', requireLogin, async (req, res) => {
 
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
   });
 
   const page = await browser.newPage();
