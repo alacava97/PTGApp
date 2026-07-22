23b2084b03ebf09c9eb63ea9a1ca85809eb9d30d
alacava97 <alacava97@gmail.com>
2026-07-22 12:10:51 -0400

    fixed class page

--- routes/classes.js
+++ routes/classes.js
@@ -11,39 +11,34 @@ router.get('/getClassList', requireLogin, async (req, res) => {
   try {
     const result = await pool.query(`
     SELECT 
-      	classes.id,
-      	classes.title,
-      	classes.level,
-      	classes.length,
-      	classes.special_equipment,
-    		classes.description,
-      	types.type AS type,
-      	sponsors.sponsor_name AS sponsor,
-      	COALESCE(
-		    jsonb_agg(
-		        jsonb_build_object(
-		            'id', instructors.id,
-		            'name',
-		                instructors.name ||
-		                CASE
-		                    WHEN instructors.rpt THEN ', RPT'
-		                    ELSE ''
-		                END
-	        	)
-		    ) FILTER (WHERE instructors.id IS NOT NULL),
-		    '[]'::jsonb
-		) AS instructors
-      	FROM classes
-      	LEFT JOIN class_instructors ON classes.id = class_instructors.class_id
-      	LEFT JOIN instructors ON instructors.id = class_instructors.instructor_id
-      	LEFT JOIN types ON types.id = classes.type
-      	LEFT JOIN sponsors ON sponsors.id = classes.sponsor_id
-      	GROUP BY classes.id, classes.title, classes.level, types.type, classes.special_equipment, sponsors.sponsor_name
-      	ORDER BY classes.title ASC;
+    	classes.*,
+    	types.type AS type,
+    	sponsors.sponsor_name AS sponsor,
+    	COALESCE(
+  	    jsonb_agg(
+  	        jsonb_build_object(
+  	            'id', instructors.id,
+  	            'name',
+  	                instructors.name ||
+  	                CASE
+  	                    WHEN instructors.rpt THEN ', RPT'
+  	                    ELSE ''
+  	                END
+          	)
+  	    ) FILTER (WHERE instructors.id IS NOT NULL),
+  	    '[]'::jsonb
+  		) AS instructors
+    	FROM classes
+    	LEFT JOIN class_instructors ON classes.id = class_instructors.class_id
+    	LEFT JOIN instructors ON instructors.id = class_instructors.instructor_id
+    	LEFT JOIN types ON types.id = classes.type
+    	LEFT JOIN sponsors ON sponsors.id = classes.sponsor_id
+    	GROUP BY classes.id, types.type, sponsors.sponsor_name
+    	ORDER BY classes.title ASC;
   `);
     res.json(result.rows);
   } catch (err) {
-    console.error(`Error fetching data from ${table}:`, err);
+    console.error(`Error fetching data from class record:`, err);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
